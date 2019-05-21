4fbb6d7df52d1997665657fda6016d7cffa7adb2
MayankECB <mayankgupta.ecb@gmail.com>
2019-05-21 10:22:46 +0530

    Update update-product.component.ts

--- src/app/update-product/update-product.component.ts
+++ src/app/update-product/update-product.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-import { Http, Response, Headers } from "@angular/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { ActivatedRoute, Router } from "@angular/router";
 import "rxjs/add/operator/toPromise";
+
+interface Product {
+  id: string;
+  color: string;
+  name: string;
+}
 
 @Component({
   selector: "app-update-product",
@@ -10,20 +16,19 @@ import "rxjs/add/operator/toPromise";
 })
 export class UpdateProductComponent implements OnInit {
   id: number;
-  data: object = {};
-  products = [];
+  data: Product;
+  products: Array<Product>;
   productObj: object = {};
   exist: boolean = false;
 
-  private headers = new Headers({ "Content-Type": "application/json" });
+  private headers = new HttpHeaders({ "Content-Type": "application/json" });
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private http: Http
+    private http: HttpClient
   ) {}
 
   updateProduct(product) {
-    debugger;
     this.productObj = {
       name: product.name,
       color: product.color
@@ -48,7 +53,8 @@ export class UpdateProductComponent implements OnInit {
     this.http
       .get("http://localhost:5555/products")
       .subscribe((res: Response) => {
-        this.products = res.json();
+        this.products = JSON.parse(JSON.stringify(res));
+        console.log(this.products);
         for (var i = 0; i < this.products.length; i++) {
           if (parseInt(this.products[i].id) === this.id) {
             this.exist = true;
